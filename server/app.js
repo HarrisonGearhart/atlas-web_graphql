944a0cb943cbf3c2f0f279b8db44b1ed780428e0
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-07-22 11:38:10 -0500

    task1

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema'); // Import the schema you created
+
+const app = express();
+
+// Use express-graphql middleware
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true  // Enables the browser UI for testing queries
+}));
+
+app.listen(4000, () => {
+  console.log('now listening for request on port 4000');
+});
