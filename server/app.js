d586d658d2ef492110ac7d4abcc834a19c0447b3
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-07-22 12:00:06 -0500

    updated app.s line 3 ./schema/schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema'); // Import the schema you created
+const schema = require('./schema/schema'); // Import the schema you created
 
 const app = express();
 
