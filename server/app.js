ccbc5509a409497c32197c5f6762e3b69e5c120b
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2025-07-24 00:06:20 -0500

    task 6 update app.js for mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
+const mongoose = require('mongoose');
 const schema = require('./schema/schema'); // Import the schema you created
 
 const app = express();
+
+const MONGO_URI = 'mongodb+srv://JoBlo:000999@cluster0.jnk7tr8.mongodb.net/taskmanager?retryWrites=true&w=majority';
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+});
 
 // Use express-graphql middleware
 app.use('/graphql', graphqlHTTP({
